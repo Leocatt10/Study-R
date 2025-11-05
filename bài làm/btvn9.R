06625be04c75cbec46b4e021948c7afa242c47a1
Nguyen Duy Manh <24000122@hus.edu.vn>
2025-11-05 22:13:18 +0700

    capnhat

--- bài làm/btvn9.R
+++ bài làm/btvn9.R
@@ -17,10 +17,20 @@ data$Dependents <- ifelse(data$Dependents == "3+", "3", data$Dependents)
 #vector có kiểu dữ liệu số, thay thế các giá trị NA bởi giá trị trung bình của vector
 #đó.
 
-colSums(is.na(data))
 
+data_empty = colSums(is.na(data))
+data_empty = names(data_empty[data_empty > 0])
 
-colSums(is.na(data))
+for(name in data_empty){
+  col = data[[name]]
+  if(is.numeric(col)){
+    
+    data[[name]][is.na(data[[name]])] = mean(data[[name]], na.rm = TRUE)
+  }else{
+    data[[name]][is.na(data[[name]])] = "missing"
+  }
+}
+
 
 #4.Thêm cột Total_Income là tổng thu nhập bằng tổng của 2 cột ApplicantIncome
 #và CoapplicantIncome sau đó xóa đi 2 cột này.
@@ -45,6 +55,31 @@ if (male > female) {
 }
  
 #6.
+X = "Male"
+Y = "Female"
+
+length_X = sum(data$Gender == X)
+length_Y = sum(data$Gender == Y)
+income_mean_test = ((mean(data$Total_Income[data$Gender == X]) 
+                     - mean(data$Total_Income[data$Gender == Y]))
+                    / sqrt(sd(data$Total_Income[data$Gender == X])^2 / length_X
+                           + sd(data$Total_Income[data$Gender == Y])^2 / length_Y))
+#7,
+
+data$Total_Income = log(data$Total_Income, base = exp(1))
+data$LoanAmount = log(data$LoanAmount, base = exp(1))
+
+data$Credit_History = ifelse(data$Credit_History > 0, 1, 0)
+
+h = -1.5453 + 3.2736 * data$Credit_History + 0.1086 * data$Total_Income - 0.2744 * data$LoanAmount
+results_predic = 1 / (1 +  exp(-h))
+data$Predict_Loan_status = ifelse(results_predic > 0.5, "Y", "N")
+
+#8, kiểm tra tính chính xác của mô hình
+table(data$Predict_Loan_status)
+accuracy = ((sum(data$Predict_Loan_status == "Y" & data$Loan_Status == "Y") 
+             + sum(data$Predict_Loan_status == "N" & data$Loan_Status == "N"))
+            / length(data$Predict_Loan_status))
 
 
 
